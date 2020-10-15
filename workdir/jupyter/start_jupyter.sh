0efbf15591419e199b084d9251d63c6bc3a2503b
Michael Wiebusch <m.wiebusch@gsi.de>
2020-10-15 11:26:08 +0200

    vim binding is enabled with special file, for those who want it

--- workdir/jupyter/start_jupyter.sh
+++ workdir/jupyter/start_jupyter.sh
@@ -3,7 +3,10 @@
 mkdir -p $(jupyter --data-dir)/nbextensions
 cp /opt/jupyter-vim-binding $(jupyter --data-dir)/nbextensions/vim_binding -R
 
-jupyter nbextension disable vim_binding/vim_binding
-#jupyter nbextension enable vim_binding/vim_binding
+if [ -e /workdir/jupyter/enable_vim_binding ]; then
+  jupyter nbextension enable vim_binding/vim_binding
+else
+  jupyter nbextension disable vim_binding/vim_binding
+fi
 
 jupyter notebook --port 8888 --no-browser --ip 0.0.0.0 --NotebookApp.token=''
