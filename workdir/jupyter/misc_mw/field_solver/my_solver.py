580b4af0639ad5e6049af69f9c7a31a64c1a2b76
Michael Wiebusch <stratomaster@gmx.net>
2022-11-22 15:46:20 +0100

    solver trace

--- workdir/jupyter/misc_mw/field_solver/my_solver.py
+++ workdir/jupyter/misc_mw/field_solver/my_solver.py
@@ -41,19 +41,35 @@ class conductor:
     
     
 class qcarrier:
-  def __init__(self, charge, conductor):
+  def __init__(self, charge, conductor, mass=1):
     self.charge = charge
     self.conductor = conductor
+    self.mass = mass
     self.x = np.random.random()*(self.conductor.x_max-self.conductor.x_min)+ self.conductor.x_min
     self.y = np.random.random()*(self.conductor.y_max-self.conductor.y_min)+ self.conductor.y_min
+    self.vx = 0
+    self.vy = 0
+    
+#def plot_particles(p_list):
+#    x = []
+#    y = []
+#    for p in p_list:
+#        x += [p.x]
+#        y += [p.y]
+#    plt.scatter(x,y)
     
 def plot_particles(p_list):
-    x = []
-    y = []
-    for p in p_list:
-        x += [p.x]
-        y += [p.y]
-    plt.scatter(x,y)
+    charges = [-1,+1]
+    
+    for charge in charges:
+        x = []
+        y = []
+        for p in p_list:
+            if p.charge == charge:
+                x += [p.x]
+                y += [p.y]
+        plt.scatter(x,y,label="q = {}".format(charge))
+    plt.legend()
     
 def q_density_from_charges(q_density,p_list):
     q_density.reset_matrix()
@@ -103,7 +119,13 @@ def e_field_from_q_density(e_field,q_density):
 def e_field_from_particles(e_field,particles):
     grid = e_field.grid
     
+    x_step = grid.x_step
+    y_step = grid.y_step
+    #diag_step = np.sqrt(x_step*x_step + y_step*y_step)
+    
     e_field.reset_matrix()
+    
+    smooth_factor = 1
     
     for ei in range(len(grid.y_edges)):
         ey = grid.y_edges[ei]
@@ -117,8 +139,14 @@ def e_field_from_particles(e_field,particles):
             
                 delta_x = particle.x - ex
                 delta_y = particle.y - ey
+                
+                if abs(delta_x) < smooth_factor*x_step:
+                    delta_x = np.sign(delta_x) * smooth_factor*x_step
+                if abs(delta_y) < smooth_factor*y_step:
+                    delta_y = np.sign(delta_y) * smooth_factor*y_step
+                
                 rr = delta_x*delta_x + delta_y*delta_y
-                rr += 1e-3
+                #rr += 1e-2
                 r = np.sqrt(rr)
                 
                 force   =  -particle.charge/rr
@@ -129,7 +157,7 @@ def e_field_from_particles(e_field,particles):
             e_field.matrix[ei,ej,0] += force_x
             e_field.matrix[ei,ej,1] += force_y
                     
-def move_particles(p_list,strength):
+def move_particles(p_list,strength,friction,granularity=0.01,vmax=1):
     new_p_list = []
     for particle in p_list:
         
@@ -144,27 +172,43 @@ def move_particles(p_list,strength):
             delta_x = other_particle.x - particle.x 
             delta_y = other_particle.y - particle.y
             rr = delta_x*delta_x + delta_y*delta_y
-            rr += 1e-3
             r = np.sqrt(rr)
+            #if r < granularity:
+            #    r = granularity
+            r += granularity
             
-            force = -strength * other_particle.charge * particle.charge/rr
+            force = -strength * other_particle.charge * particle.charge/(r*r)
             force_x += force * delta_x/r
             force_y += force * delta_y/r
             
         new_particle = copy.copy(particle)
         
-        new_particle.x += force_x
-        new_particle.y += force_y
+        new_particle.x += new_particle.vx
+        new_particle.y += new_particle.vy
+        
+        new_particle.vx += force_x - friction*new_particle.vx
+        new_particle.vy += force_y - friction*new_particle.vy
+        
+        v = np.sqrt(new_particle.vx*new_particle.vx +new_particle.vy * new_particle.vy)
+        
+        if v > vmax:
+            new_particle.vx = new_particle.vx/v * vmax
+            new_particle.vy = new_particle.vy/v * vmax
+        
         
         if new_particle.x > new_particle.conductor.x_max:
             new_particle.x = new_particle.conductor.x_max
+            new_particle.vx = 0
         elif new_particle.x < new_particle.conductor.x_min:
             new_particle.x = new_particle.conductor.x_min
+            new_particle.vx = 0
             
         if new_particle.y > new_particle.conductor.y_max:
             new_particle.y = new_particle.conductor.y_max
+            new_particle.vy = 0
         elif new_particle.y < new_particle.conductor.y_min:
             new_particle.y = new_particle.conductor.y_min
+            new_particle.vy = 0
             
         new_p_list += [new_particle]
     return new_p_list
